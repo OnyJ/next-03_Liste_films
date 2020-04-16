b3c46e9b8c7236865d20fe8de6f99213585921a7
OnyJ <julien.josselin75@hotmail.fr>
2020-04-16 20:16:06 +0200

    observer working. Better modal.

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // Movie consts
 const MOVIE_RESEARCH_URL = `http://www.omdbapi.com/?i=tt3896198&apikey=${key}`;
 const MOVIE_DISPLAY = document.getElementById("displayMovies");
+
 // Description consts (modal)
-const DESCRIPTION_PARAGRAPH = document.getElementById("modalDescription");
 const MODAL_VIEW = document.getElementById("descriptionModal");
+const MODAL_CONTENT = document.getElementById("modalContent");
 const CLOSE_MODAL = document.getElementsByClassName("close")[0];
 
 // Movie Request By User
@@ -21,10 +22,33 @@ searchMovie = (movie) => {
   fetch(newUrl)
     .then((response) => response.json())
     .then((response) => {
+      MOVIE_DISPLAY.innerHTML = "";
       const foundMovies = response.Search;
       console.log(response);
       foundMovies.forEach((movie) => {
         showMovie(movie.Poster, movie.Title, movie.Year, movie.imdbID);
+      });
+      return response;
+    })
+    .then((response) => {
+      let observer = new IntersectionObserver(
+        function (cards) {
+          cards.forEach(function (card) {
+            if (card.intersectionRatio > 0.3) {
+              card.target.classList.remove("not-visible");
+              console.log("Item visible");
+            }
+          });
+        },
+        {
+          threshold: [0.5],
+        }
+      );
+
+      let items = document.querySelectorAll(".card");
+      items.forEach(function (item) {
+        item.classList.add("not-visible");
+        observer.observe(item);
       });
     })
     .catch((error) => console.error(error));
@@ -33,19 +57,19 @@ searchMovie = (movie) => {
 // ShowMovie
 showMovie = (image, title, year, id) => {
   MOVIE_DISPLAY.innerHTML += `
-    <div class="card mr-3 mt-5 col-lg-3 p-0" style="width: 18rem;">
+    <div class="card mr-3 mt-5 col-lg-5 p-0" style="width: 18rem;">
       <img class="card-img-top" src="${image}" alt="Card image cap">
       <div class="card-body">
         <p class="h4">${title}</p>
         <p class="card-text">${year}</p>
       </div>
-      <button type="button" class="btn btn-primary" onclick="seeMoreIsClicked('${id}')" id="${id}">See more</button>
+      <button type="button" class="btn btn-primary" onclick="seeMoreIsClicked('${image}', '${title}', '${year}', '${id}')" id="${id}">See more</button>
     </div>
   `;
 };
 
 // Modal system (to research movie description)
-seeMoreIsClicked = (id) => {
+seeMoreIsClicked = (image, title, year, id) => {
   const DESCRIPTION_RESEARCH_URL = `http://www.omdbapi.com/?apikey=${key}&i=${id}`;
 
   fetch(DESCRIPTION_RESEARCH_URL)
@@ -55,15 +79,24 @@ seeMoreIsClicked = (id) => {
       return response;
     })
     .then((response) => {
-      const DESCRIPTION = response.Plot;
-      showDescription(DESCRIPTION);
+      let description = response.Plot;
+      showDescription(image, title, year, description);
     })
     .catch((error) => console.error(error));
 };
 
-showDescription = (DESCRIPTION) => {
+showDescription = (image, title, year, description) => {
   MODAL_VIEW.style.display = "block";
-  DESCRIPTION_PARAGRAPH.innerHTML = `${DESCRIPTION}`;
+  MODAL_CONTENT.innerHTML = `
+    <div class="col-sm">
+      <img src="${image}" alt="No image" />
+    </div>
+    <div class="col-lg-8 text-justify">
+      <h1>${title}</h1>
+      <h4 class="text-right mr-5">${year}</h4>
+      <p class="container mt-5 pr-5 h4">${description}</p>
+    </div>
+`;
 };
 closeDescription = () => {
   MODAL_VIEW.style.display = "none";
@@ -73,5 +106,3 @@ window.onclick = (event) => {
     MODAL_VIEW.style.display = "none";
   }
 };
-
-// Intersection observer : dans showMovie ?
